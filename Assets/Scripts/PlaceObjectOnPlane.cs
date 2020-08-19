04c32817c61113a380d4878b6cc20f74971e54c5
Mayank Raj <AgrMayank@outlook.com>
2020-08-20 00:03:04 +0530

    Add Auto Place On Plane Script

---
+++ Assets/Scripts/PlaceObjectOnPlane.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARPlaneManager))]
+public class PlaceObjectOnPlane : MonoBehaviour 
+{
+    public GameObject placedPrefab;
+
+    private GameObject placedObject;
+
+    public ARPlaneManager planeManager;
+
+    private void Awake() 
+    {
+        planeManager = GetComponent<ARPlaneManager>();
+        planeManager.planesChanged += PlaneChanged;
+    }
+
+    private void PlaneChanged(ARPlanesChangedEventArgs args)
+    {
+        if(args.added != null && placedObject == null)
+        {
+            ARPlane arPlane = args.added[0];
+            placedObject = Instantiate(placedPrefab, arPlane.transform.position, Quaternion.identity);
+        }
+    }
+}
