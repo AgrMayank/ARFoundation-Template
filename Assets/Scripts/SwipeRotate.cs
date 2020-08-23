ad40fd7fd425ea7ecef57da9125830d3d765abe9
Mayank Raj <AgrMayank@outlook.com>
2020-08-23 21:02:02 +0530

    Add Scripts
    CameraFacing.cs
    SwipeRotate.cs

---
+++ Assets/Scripts/SwipeRotate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+ 
+public class SwipeRotate : MonoBehaviour
+{
+    private Touch touch;
+    private Vector2 touchPosition;
+    private Quaternion rotXY;
+
+    public float rotateSpeedX = 0.1f;
+    public float rotateSpeedY = 0.1f;
+    
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Moved)
+            {
+                rotXY = Quaternion.Euler(touch.deltaPosition.y * rotateSpeedX, - touch.deltaPosition.x * rotateSpeedY, 0f);
+                transform.rotation = rotXY * transform.rotation;
+            }
+        }
+    }
+}
