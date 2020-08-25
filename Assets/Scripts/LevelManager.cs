2792d291a155e6b510139d53214fc97daa63db5d
Mayank Raj <AgrMayank@outlook.com>
2020-08-25 17:29:20 +0530

    Add Scripts
    1. LevelManager.cs
    2. OpenURL.cs

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public static string levelToLoad;
+    public GameObject CrossfadeAnim;
+
+    private float _progress = 0f;
+
+    public void LoadLevel(string levelName)
+    {
+        levelToLoad = levelName;
+        Debug.Log("New Level Load : " + levelToLoad);
+
+        CrossfadeAnim.SetActive(true);
+        StartCoroutine(LoadYourAsyncScene());
+    }
+
+    IEnumerator LoadYourAsyncScene()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(levelToLoad);
+        asyncLoad.allowSceneActivation = false;
+
+        while (asyncLoad.progress < 0.9F)
+        {
+            yield return null;
+        }
+
+        Debug.Log(asyncLoad.progress);
+        yield return new WaitForSeconds(0.8f);
+
+        // while (!_isClicked) // While condition == false
+        // {
+        //     yield return null;
+        // }
+
+        asyncLoad.allowSceneActivation = true;
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit Requested!");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/OpenURL.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenURL : MonoBehaviour
+{
+    public string URL = "https://agrmayank.github.io";
+    public void OpenURLInBrowser()
+    {
+        Application.OpenURL(URL);
+    }
+}
